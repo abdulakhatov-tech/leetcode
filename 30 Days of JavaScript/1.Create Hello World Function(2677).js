7c9b06f2433c074fe6b2068797be6639ac0da39b
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-12-12 05:37:48 +0500

    30 days of javascript & createHelloWorld

---
+++ 30 Days of JavaScript/1.Create Hello World Function(2677).js
@@ -0,0 +1,12 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+function createHelloWorld() {
+  return function () {
+    return 'Hello World';
+  };
+}
+
+const f = createHelloWorld();
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
