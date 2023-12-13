e9ef507ec96d6ce8d758cc780c48c89995829d7f
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-12-13 19:54:23 +0500

    easy: 1.two sum

--- easy/1. Two Sum.js
+++ easy/1. Two Sum.js
@@ -7,10 +7,19 @@
 function twoSum(nums, target) {
   let result = 0;
 
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (result === target) {
+  //       result = nums[i] + nums[i + 1];
+  //       return [i, i + 1];
+  //     }
+  //   }
+
   for (let i = 0; i < nums.length; i++) {
-    result = nums[i] + nums[i + 1];
-    if (result === target) {
-      return [i, i + 1];
+    for (let j = i + 1; j < nums.length; j++) {
+      result = nums[i] + nums[j];
+      if (result === target) {
+        return [i, j];
+      }
     }
   }
 }
@@ -18,4 +27,6 @@ function twoSum(nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+console.log(twoSum([3, 2, 3], 6));
 console.log(twoSum([3, 3, 4, 2, 5, 1, 6, 9, 6], 15));
+console.log(twoSum([2, 5, 5, 11], 10));
