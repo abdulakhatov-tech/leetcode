73b102fbece22f7fb58c2c54d12023963921f6de
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-02-18 09:13:20 +0500

    405. Convert a Number to Hexadecimal

---
+++ easy/405. Convert a Number to Hexadecimal.js
@@ -0,0 +1,14 @@
+function toHex(num) {
+  if (num === 0) return '0';
+
+  if (num < 0) {
+    // For negative numbers, compute the two's complement representation
+    num = 0xffffffff + num + 1;
+  }
+
+  return num.toString(16);
+}
+
+// let result = toHex(26);
+let result = toHex(-1);
+console.log(result);
