4808a0ff1b254d3d3a3dc2f318958747b083bac8
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-04-27 07:11:47 +0500

    dasturlash.uz: if: berilgan yil kabisa yilimi

--- dasturlash.uz/if/script.js
+++ dasturlash.uz/if/script.js
@@ -114,3 +114,19 @@
 // }
 
 // ===================================================================================
+// #9
+// Berilgan yil kabisa yilmi
+
+// Berilgan yill kabisa yili ekanligini aniqlang. Agar kabisa yili bo'lsa true  konsolga chiqsin.
+// Agar bo'linmasa false  konsolga chiqsin.
+
+// Kabisa yil deb 4 yilda birmarta keladigan yilga aytiladi. Shu yili kunlar soni 365 emas 366 ga teng bo'ladi.
+// Bu yili fevral oyida 29 kun bo'ladi.
+
+// let year = 2000;
+// if (year % 4 === 0) {
+//    console.log(true);
+// } else {
+//    console.log(false);
+// }
+// ===================================================================================
