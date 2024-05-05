30e390544975e5ab143517963bd0fd12cf3ec895
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-05-05 18:27:03 +0500

    free_time: #3

--- free_time/script.js
+++ free_time/script.js
@@ -20,12 +20,31 @@
 // #2
 // Given a word, write a function that returns the first index and the last index of a character.
 
-function charIndex(word, character) {
-   let firstIndex = word.indexOf(character);
-   let lastIndex = word.lastIndexOf(character);
+// function charIndex(word, character) {
+//    let firstIndex = word.indexOf(character);
+//    let lastIndex = word.lastIndexOf(character);
 
-   return [firstIndex, lastIndex];
-}
+//    return [firstIndex, lastIndex];
+// }
 
-console.log(charIndex("hello", "l"));
-console.log(charIndex("circumlocution", "c"));
+// console.log(charIndex("hello", "l"));
+// console.log(charIndex("circumlocution", "c"));
+
+// ==============================================================================
+// #3
+// Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+
+// function removeDups(arr) {
+//    let newArr = [];
+
+//    for (let i = 0; i < arr.length; i++) {
+//       if (!newArr.includes(arr[i])) {
+//          newArr = [...newArr, arr[i]];
+//       }
+//    }
+
+//    return newArr;
+// }
+
+// console.log(removeDups([1, 0, 1, 0]));
+// console.log(removeDups(["The", "big", "cat"]));
