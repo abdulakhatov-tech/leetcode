c0e63ed2eb19cf1cba63a9350a657a61793447d4
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-05-05 18:22:53 +0500

    free_time: #1

---
+++ free_time/script.js
@@ -0,0 +1,17 @@
+// ==============================================================================
+// #1
+// Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
+
+// function spelling(word) {
+//    let result = "";
+//    let resArr = [];
+
+//    for (let i = 0; i < word.length; i++) {
+//       result += word[i];
+//       resArr.push(result);
+//    }
+
+//    return resArr;
+// }
+
+// console.log(spelling("bee"));
