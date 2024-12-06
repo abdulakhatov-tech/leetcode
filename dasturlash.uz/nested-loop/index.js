0968b33a0547d528ab5e74a7035d38bd931c1cff
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-12-06 06:50:19 +0500

    feat(dasturlash.uz): added 15.togriburchak-qolibi

--- dasturlash.uz/nested-loop/index.js
+++ dasturlash.uz/nested-loop/index.js
@@ -16,4 +16,27 @@
 // console.log(fn(5))
 
 
+// 15. To'rtburchak qolibi
 
+const fn = (n) => {
+  let result = '';
+
+  for(let i = 1; i <= n; i++){
+    for(let k = 1; k <= n; k++) {
+      if(i === 1 || i=== n) {
+        result += '*'
+      } else {
+        if(k === 1 || k === n) {
+          result += '*'
+        } else {
+          result += ' '
+        }
+      }
+    }
+    result += '\n'
+  }
+
+  return result
+}
+
+console.log(fn(5))
