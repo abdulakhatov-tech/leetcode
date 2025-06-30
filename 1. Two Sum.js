e3dd94bf28c2cea8c5928aea72e20c7c26fbe021
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-06-30 14:55:32 +0500

    feat: 1. Two Sum

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,18 +1,18 @@
-const twoSum = (nums, target) => {
+
+function twoSum(nums, target) {
     const map = new Map();
 
     for(let i = 0; i < nums.length; i++) {
-        const subtraction = target - nums[i];
+        const subscription = target - nums[i];
 
-        if(map.has(subtraction)) {
-            return [map.get(subtraction), i]
+        if(map.has(subscription)) {
+            return [map.get(subscription), i];
         } else {
             map.set(nums[i], i)
         }
     }
-
 }
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3,3], 6));
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+console.log(twoSum([3, 3], 6)); // Output: [0, 1]
