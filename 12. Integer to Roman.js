1c5c701cd3ed49c5c15d835b23bb8f250fe7a48e
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-06-30 10:45:32 +0500

    feat: 12. Integer to Roman

---
+++ 12. Integer to Roman.js
@@ -0,0 +1,34 @@
+
+const intToRoman = (num) => {
+    const valMap = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+
+    let roman = "";
+
+    for (const [value, symbol] of valMap) {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
+
+}
+
+console.log(intToRoman(3)); // Output: "III"
+console.log(intToRoman(58)); // Output: "LVIII"
+console.log(intToRoman(1994)); // Output: "MCMXCIV"
